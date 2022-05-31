68973f619a623cd9c2d161fb908abc318eca0596
roses360 <diegolerose360@outlook.it>
2022-05-31 18:49:32 +0200

    Update Window.java

--- src/GUI/Window.java
+++ src/GUI/Window.java
@@ -1,14 +1,29 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Window{
+
+    JFrame frame;
+    //JPanel panel;
+
     public Window(ClassPanel panel){
-        JFrame frame = new JFrame(panel.title);
+        //this.panel = panel;
+        frame = new JFrame(panel.title);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         //set the start panel as content pane
+        
         frame.setContentPane(panel);
         frame.pack();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
+    }
+
+    //method to refresh window: it replaces old panel with a new one
+    public void replaceContent(ClassPanel newPanel){
+        frame.getContentPane().removeAll();
+        frame.setContentPane(newPanel);
+        frame.revalidate();
+        frame.repaint();
     }
 }
