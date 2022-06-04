d6941bd1e913e324223b15af0767f5ad477e6af6
Bretzz <89346215+Bretzz@users.noreply.github.com>
2022-06-04 09:12:11 +0200

    added editTopics

--- src/GUI/PlusPanel.java
+++ src/GUI/PlusPanel.java
@@ -1,3 +1,12 @@
+import Common.Message;
+import Common.Message.MessageType;
+import Client.ClientRun;
+
 public class PlusPanel extends ClassPanel {
-    
+    static void editTopics(String t){
+        String[] ts = t.split(" ");
+        Message m = new Message("",MessageType.UPDATE,ts,"");
+        ClientRun cr = new ClientRun();
+        cr.sendMessage(m);
+    }
 }
