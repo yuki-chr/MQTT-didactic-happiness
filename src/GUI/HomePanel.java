832607addbd03831d4801885d0a395bbba31fc3b
roses360 <diegolerose360@outlook.it>
2022-06-05 16:17:03 +0200

    homepanel settings

--- src/GUI/HomePanel.java
+++ src/GUI/HomePanel.java
@@ -3,8 +3,9 @@ import java.awt.*;
 
 public class HomePanel extends ClassPanel{
 
-    JPanel description, editServ;
-    String text;
+    JPanel description, servSpace;
+    JButton startServer_btn;
+    String text = "Hello World!";
     final static String newline = "\n";
 
     public HomePanel(){
@@ -13,10 +14,10 @@ public class HomePanel extends ClassPanel{
         this.setLayout(new FlowLayout());
 
         description = new JPanel();
-        editServ = new JPanel();
+        servSpace = new JPanel();
 
         writeDescription();
-        editServPanel();
+        startServer();
         
         
     }
@@ -30,11 +31,29 @@ public class HomePanel extends ClassPanel{
         textArea.setFont(new Font("SansSerif", Font.BOLD, 18));
         textArea.setEditable(false);
         textArea.append(text);
-        
-        //some code 
+
+        description.setLayout(new BorderLayout());
+        description.add(welcome, BorderLayout.NORTH);
+        description.add(textArea, BorderLayout.CENTER);
+        //some code
+
     }
 
-    public void editServPanel(){
+    public void startServer(){
+        JLabel label = new JLabel("Lancia il server!", SwingConstants.CENTER);
+        label.setFont(new Font("SansSerif", Font.BOLD, 18));
+
+        //btn
+        startServer_btn = new JButton("Start");
+
+        //add to panel
+        JPanel p = new JPanel();
+        p.setLayout(new BorderLayout());
+        p.add(label, BorderLayout.NORTH);
+        p.add(startServer_btn, BorderLayout.CENTER);
+
+        servSpace.setLayout(new BorderLayout());
+        servSpace.add(p, BorderLayout.CENTER);
 
     }
 
