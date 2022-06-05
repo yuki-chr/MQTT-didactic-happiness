1bdcaf69be8b8decef3683b57e445b3e0dedbc91
Ender1806 <harveyrafanan2@gmail.com>
2022-06-05 15:57:01 +0200

    asd

--- src/Client/ClientRun.java
+++ src/Client/ClientRun.java
@@ -1,18 +1,18 @@
 package Client;
-import java.util.*;
 import java.io.*;
 import java.net.*;
 
 public class ClientRun{
-    public static void main(String[] args){
+    public static void main(String[] args) throws UnknownHostException{
         //Scanner in = new Scanner(System.in());
-        System.out.print("Scrivi qualcosa : ");
         String a = "Ciao";
-        String ServerIP = "192.168.1.254";
-        byte[] msg = new byte[1024];
-        msg = a.getBytes();
-        DatagramSocket socket = new DatagramSocket(6789);
-        DatagramPacket packet = new DatagramPacket(msg,msg.length,IP,4445);
-        socket.send(packet);
+        InetAddress ServerIP = InetAddress.getLocalHost();
+        byte[] msg = a.getBytes();
+        try (DatagramSocket socket = new DatagramSocket(6789)) {
+            DatagramPacket packet = new DatagramPacket(msg,msg.length,ServerIP,4445);
+            socket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
