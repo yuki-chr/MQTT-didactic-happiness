e6e8951cbe8951db085c4d64597b4291c91e9be7
Bretzz <89346215+Bretzz@users.noreply.github.com>
2022-06-05 15:38:00 +0200

    aolo

--- src/Client/ClientRun.java
+++ src/Client/ClientRun.java
@@ -1,18 +1,20 @@
 package Client;
+
 import java.util.*;
 import java.io.*;
 import java.net.*;
 
 public class ClientRun{
     public static void main(String[] args){
-        //Scanner in = new Scanner(System.in());
+        Scanner in = new Scanner(System.in);
         System.out.print("Scrivi qualcosa : ");
-        String a = "Ciao";
-        String ServerIP = "192.168.1.254";
+        String txt = in.nextLine();
+        //String txtIP = "192.168.1.254";
+        InetAddress ServerIP = InetAddress.getByName("192.168.1.254");
         byte[] msg = new byte[1024];
-        msg = a.getBytes();
+        msg = txt.getBytes();
         DatagramSocket socket = new DatagramSocket(6789);
-        DatagramPacket packet = new DatagramPacket(msg,msg.length,IP,4445);
+        DatagramPacket packet = new DatagramPacket(msg,msg.length,ServerIP,4445);
         socket.send(packet);
     }
 }
