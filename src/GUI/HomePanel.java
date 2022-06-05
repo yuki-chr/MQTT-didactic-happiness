88aae608e86d8cbdb1ea9c54bc6669732cfee22c
roses360 <diegolerose360@outlook.it>
2022-06-05 20:23:32 +0200

    Update HomePanel.java

--- src/GUI/HomePanel.java
+++ src/GUI/HomePanel.java
@@ -16,6 +16,7 @@ public class HomePanel extends ClassPanel{
 
     JPanel description, servSpace;
     JButton startServer_btn, connectServer_btn;
+    JTextField ip_input, port_input;
     String text = "Hello World!";
     final static String newline = "\n";
     ServerRun serverRun;
@@ -25,52 +26,102 @@ public class HomePanel extends ClassPanel{
         this.title ="Home";
         this.setLayout(new FlowLayout());
 
-        
+
 
         //description panel
-        description = new JPanel();
-        description.setBackground(Util.bg);
-        description.setSize(Util.width-50, Util.height-50);
-        
-        
-        servSpace = new JPanel();
-        JLabel welcome = new JLabel("Benvenuto!", SwingConstants.CENTER);
-        welcome.setFont(new Font("SansSerif", Font.BOLD, 24));
-        welcome.setBackground(Util.bg);
+            description = new JPanel();
+            description.setBackground(Util.bg);
+            description.setSize(Util.width-50, Util.height-50);
+            
+            
+            
+            JLabel welcome = new JLabel("Benvenuto!", SwingConstants.CENTER);
+            welcome.setFont(new Font("SansSerif", Font.BOLD, 24));
+            welcome.setBackground(Util.bg);
 
-        JTextArea textArea = new JTextArea(5, 20);
-        textArea.setFont(new Font("SansSerif", Font.BOLD, 18));
-        textArea.setBorder(BorderFactory.createLineBorder(Color.black));
-        textArea.setEditable(false);
-        textArea.append(text);
+            JTextArea textArea = new JTextArea(5, 20);
+            textArea.setFont(new Font("SansSerif", Font.BOLD, 18));
+            textArea.setBorder(BorderFactory.createLineBorder(Color.black));
+            textArea.setEditable(false);
+            textArea.append(text);
 
-        description.setLayout(new BorderLayout());
-        description.add(welcome, BorderLayout.NORTH);
-        description.add(textArea, BorderLayout.CENTER);
+            description.setLayout(new BorderLayout());
+            description.add(welcome, BorderLayout.NORTH);
+            description.add(textArea, BorderLayout.CENTER);
 
         //server part
-        JLabel launch = new JLabel("Lancia il server!", SwingConstants.CENTER);
-        launch.setFont(new Font("SansSerif", Font.BOLD, 18));
-        launch.setBackground(Util.bg);
+            servSpace = new JPanel();
+            servSpace.setLayout(new BorderLayout());
 
-        startServer_btn = new JButton("Start");
-        startServer_btn.addActionListener(this);
-        connectServer_btn = new JButton("Connect");
-        connectServer_btn.addActionListener(this);
-        JPanel btn_p = new JPanel();
-        btn_p.setBackground(Util.bg);
-        btn_p.add(startServer_btn);
-        btn_p.add(connectServer_btn);
+            //set server
+                JPanel connectP = new JPanel();
+                connectP.setLayout(new BorderLayout());
+                connectP.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        JPanel p = new JPanel();
-        p.setBackground(Util.bg);
-        p.setLayout(new BorderLayout());
-        p.add(launch, BorderLayout.CENTER);
-        p.add(btn_p, BorderLayout.SOUTH);
+                JLabel l1 = new JLabel("Collegati a un server!", SwingConstants.CENTER);
+                l1.setFont(new Font("SansSerif", Font.BOLD, 16));
+                l1.setBackground(Util.bg);
+                connectP.add(l1, BorderLayout.NORTH);
 
-        servSpace.setLayout(new BorderLayout());
-        servSpace.add(p, BorderLayout.CENTER);
+                ip_input = new JTextField(10);
+                port_input = new JTextField(10);
+                connectServer_btn = new JButton("Connetti");
+                
 
+                // Add labelled input fields to display
+                JPanel inFieldPane = new JPanel();
+                inFieldPane.setLayout(new GridLayout(2,2));
+                inFieldPane.add(new JLabel("IP: "));
+                inFieldPane.add(ip_input);
+                ip_input.addActionListener(this);
+                inFieldPane.add(new JLabel("Port: "));
+                inFieldPane.add(port_input);
+                port_input.addActionListener(this);
+                connectP.add(inFieldPane, BorderLayout.CENTER);
+
+                // Add submission button
+                JPanel submitPane = new JPanel();
+                submitPane.setLayout(new FlowLayout());
+                connectServer_btn.addActionListener(this);
+                submitPane.add(connectServer_btn);
+                connectP.add(submitPane, BorderLayout.SOUTH);
+            
+                // Add Output fields
+                /*
+                * check se esiste veramente il server, guardare i ping
+                */
+
+            //create server
+                JPanel createP = new JPanel();
+                createP.setLayout(new BorderLayout());
+                createP.setBorder(BorderFactory.createLineBorder(Color.black));
+
+                JLabel l2 = new JLabel("Crea un server!", SwingConstants.CENTER);
+                l2.setFont(new Font("SansSerif", Font.BOLD, 16));
+                l2.setBackground(Util.bg);
+                createP.add(l2, BorderLayout.NORTH);
+
+                //ip_output = new JTextField(10);
+                //port_output = new JTextField(10);
+                startServer_btn = new JButton("Start");
+
+                // Add submission button
+                JPanel submitPane2 = new JPanel();
+                submitPane2.setLayout(new FlowLayout());
+                startServer_btn.addActionListener(this);
+                submitPane2.add(startServer_btn);
+                createP.add(submitPane2, BorderLayout.CENTER);
+
+                // Add Output fields
+                /*
+                * check se esiste veramente il server, guardare i ping
+                */
+
+        
+            servSpace.add(connectP, BorderLayout.NORTH);
+            servSpace.add(createP, BorderLayout.CENTER);
+            
+        
         //add to big panel   
         this.add(description);
         this.add(servSpace);
