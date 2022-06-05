6cb40281891b5a9db32c60f5a441fbaad32e2881
yuki.chr <lorebarza@libero.it>
2022-06-05 15:59:28 +0200

    fixed (maybe) ClientRun

--- src/Client/ClientRun.java
+++ src/Client/ClientRun.java
@@ -1,7 +1,6 @@
 package Client;
 
 import java.util.*;
-import java.io.*;
 import java.net.*;
 
 public class ClientRun{
@@ -9,12 +8,20 @@ public class ClientRun{
         Scanner in = new Scanner(System.in);
         System.out.print("Scrivi qualcosa : ");
         String txt = in.nextLine();
-        //String txtIP = "192.168.1.254";
-        InetAddress ServerIP = InetAddress.getByName("192.168.1.254");
         byte[] msg = new byte[1024];
         msg = txt.getBytes();
-        DatagramSocket socket = new DatagramSocket(6789);
-        DatagramPacket packet = new DatagramPacket(msg,msg.length,ServerIP,4445);
-        socket.send(packet);
+        //String txtIP = "192.168.1.254";
+
+        InetAddress ServerIP;
+        DatagramSocket socket;
+        try{
+            ServerIP = InetAddress.getByName("79.52.133.115");
+            socket = new DatagramSocket(6789);
+            DatagramPacket packet = new DatagramPacket(msg,msg.length,ServerIP,4445);
+            socket.send(packet);
+        }catch(Exception e){
+            System.out.println("problems with sending stuff: " + e);
+        }
+        in.close();
     }
 }
