f901cd0cd20bd96f2eed2e74a0089522cd399997
Ender1806 <harveyrafanan2@gmail.com>
2022-06-05 18:47:43 +0200

    asd

--- src/Client/ClientRun.java
+++ src/Client/ClientRun.java
@@ -1,25 +1,52 @@
 package Client;
+import Common.*;
 
 import java.util.*;
+import java.io.IOError;
+import java.io.IOException;
 import java.net.*;
 
-public class ClientRun{
+public class ClientRun extends Thread{
     InetAddress ip;
     int port;
+    DatagramSocket socket;
+    private boolean running;
+    private byte[] buf = new byte[256];
+
+    public void sendMessage(Message MSG) throws IOException{
+        byte[] msg = MSG.serialize().getBytes();
+        DatagramPacket Out = new DatagramPacket(msg,msg.length,ip,port);
+        socket.send(Out);
+    }
+
+    @Override
+    public void run(){
+        running = true;
+        while (running){
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+        try{
+            socket.receive(packet);
+            String a = packet.toString();
+            System.out.println("Server : " + a);
+        }catch(Exception e){
+            running = false;
+        }
+        }
+    }
+
+
+
+
     public static void main(String[] args){
+        ClientRun ClientRun = new ClientRun(ip, port);
         Scanner in = new Scanner(System.in);
         System.out.print("Scrivi qualcosa : ");
         String txt = in.nextLine();
         byte[] msg = new byte[1024];
         msg = txt.getBytes();
         //String txtIP = "192.168.1.254";
-
-        InetAddress ServerIP;
-        DatagramSocket socket;
-        try{
-            ServerIP = InetAddress.getByName("79.52.133.115");
-            socket = new DatagramSocket(6789);
-            DatagramPacket packet = new DatagramPacket(msg,msg.length,ServerIP,4445);
+         try{
+            DatagramPacket packet = new DatagramPacket(msg,msg.length,ip,port);
             socket.send(packet);
         }catch(Exception e){
             System.out.println("problems with sending stuff: " + e);
